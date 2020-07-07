ef32c2ca3d8c936a259347d4c33e31efb380fc13
Zarak <zarak@hotmail.ca>
2020-07-07 16:15:26 +0500

    Initialize empty board

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,21 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
+
+data Three = 
+        One 
+      | Two 
+      | Three 
+      deriving (Show, Eq, Ord, Enum)
+
+newtype TicTacToe a = TicTacToe2
+    { board :: Three -> Three -> a 
+    }
+
+instance Show (TicTacToe a) where
+    show (TicTacToe2 a) = "test"
+
+emptyBoard :: TicTacToe (Maybe Bool)
+emptyBoard = TicTacToe2 $ const $ const Nothing
+
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
