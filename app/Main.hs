b82895a01c823174b51ac13b94650e538ef6787c
Zarak <7254237+zarak@users.noreply.github.com>
2020-07-20 13:07:48 +0500

    Add game flow IO

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,28 @@
 module Main where
 
 import GameState
+import Control.Monad (forever, when)
+import System.Exit (exitSuccess)
+import System.Random (randomRIO)
 
-testBoard = Board "XXO X O  "
+testState = GameState (Board "XXO X O  ") X
+
+handleMove :: GameState Board Player -> Int -> IO (GameState Board Player)
+handleMove game idx = do
+    print "Make a move"
+    return (move game idx)
+
+gameOver :: GameState Board Player -> IO ()
+gameOver game = when (isEnd game) exitSuccess
+
+runGame :: GameState Board Player -> IO ()
+runGame game = forever $ do
+    gameOver game
+    print game
+    move <- getLine
+    handleMove game (read move :: Int) >>= runGame
 
 main :: IO ()
-main = putStr $ "Test Board\n" ++ show testBoard
-
+main = do
+    runGame initBoard
+    --runGame testState
